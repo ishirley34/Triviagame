a4f979529cd83425517bccaffa435c7ac4d4cbb2
ian shirley <ian shirley>
2016-11-19 22:28:21 -0600

    question array skeleton created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,26 @@
 // Remember to look at the week 6 day 2 things in the class repo for examples on the buttons.
 
-// This is the question pool that will be used to populate the question box in the main page.
-var questions = {
 
-};
+
 // This is the main game object
 var cartoonTrivia = {
 
+	// This is the question pool that will be used to populate the question box in the main page.
+	questions: {
+		question: [
+			"answer1",
+			"answer2",
+			"answer3",
+			"answer4"
+		],
+		question: [
+		"answer1",
+		"answer2",
+		"answer3",
+		"answer4"]
+	},
 	// declaration of game variables
-	startTime: 120, 
+	startTime: 10, 
 
 	counter: function() {
 		setInterval(cartoonTrivia.decrement, 1000);
@@ -16,17 +28,23 @@ var cartoonTrivia = {
 
 	// timer function for the trivia clock
 	decrement: function() {
+		// this decrements the timer
 		cartoonTrivia.startTime--;
+		// This prints the timer on the html
 		$("#timerBox").html("<h2>" + cartoonTrivia.startTime + "</h2>");
-			console.log(cartoonTrivia.startTime);
+
 		// If loop to end game when the timer reaches 0
 		if (cartoonTrivia.startTime == 0) {
-			cartoonTrivia.counter.stop();
+			// This stops the timer at 0
+			cartoonTrivia.stop();
 
 			gameOver();
 		} // closes the timer if
-
 	}, // Closes the timer function
+
+	stop: function(){
+        clearInterval(counter);
+    }, //closes the stop function
 
 	// This function is for the start button
 	startButton: function() {
@@ -34,5 +52,6 @@ var cartoonTrivia = {
 		$("#timerBox").fadeIn(4000);
 		cartoonTrivia.counter();
 	}
+
 
 }; // Closes the cartoonTrivia obj
