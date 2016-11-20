af3c314d8260a6a9a71194a7a48e4a94a9b244ea
ian shirley <ian shirley>
2016-11-20 11:36:22 -0600

    timer fixed, more JS started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,34 +14,55 @@ var cartoonTrivia = {
 			"answer4"
 		],
 		question: [
-		"answer1",
-		"answer2",
-		"answer3",
-		"answer4"]
+			"answer1",
+			"answer2",
+			"answer3",
+			"answer4"
+		],
+		question: [
+			"answer1",
+			"answer2",
+			"answer3",
+			"answer4"
+		],
+		question: [
+			"answer1",
+			"answer2",
+			"answer3",
+			"answer4"
+		],
 	},
-	// declaration of game variables
-	startTime: 10, 
 
+	// declaration of game variables
+	startTime: 10,
+	correct: 0,
+	incorrect: 10,
+
+	// 
 	counter: function() {
 		setInterval(cartoonTrivia.decrement, 1000);
-	},
+	}, // closes the counter function
 
 	// timer function for the trivia clock
 	decrement: function() {
+
 		// this decrements the timer
 		cartoonTrivia.startTime--;
+
 		// This prints the timer on the html
 		$("#timerBox").html("<h2>" + cartoonTrivia.startTime + "</h2>");
 
 		// If loop to end game when the timer reaches 0
 		if (cartoonTrivia.startTime == 0) {
+
 			// This stops the timer at 0
 			cartoonTrivia.stop();
 
 			gameOver();
-		} // closes the timer if
+		} // closes the timer if	
 	}, // Closes the timer function
 
+	// This function stops the counter
 	stop: function(){
         clearInterval(counter);
     }, //closes the stop function
@@ -51,7 +72,7 @@ var cartoonTrivia = {
 		$("#quizStart").fadeOut(4000);
 		$("#timerBox").fadeIn(4000);
 		cartoonTrivia.counter();
-	}
+	}, // closes start button
 
 
 }; // Closes the cartoonTrivia obj
